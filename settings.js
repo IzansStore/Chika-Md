80e81127226139719e1bef73bdaf14fd6f0c3c3c
IzansStore <115550578+IzansStore@users.noreply.github.com>
2023-04-11 17:54:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,9 +27,9 @@ global.premium = ['6282298627146'] //ubah jadi nomor mu, note tanda ' gausah di 
 global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Zaa' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'IzansMd' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Zaa' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'FazaMd' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Zaabot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'FazaMd' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -41,8 +41,8 @@ global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© IzansMd' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Zaa' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Zaabot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'FazaMd' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
